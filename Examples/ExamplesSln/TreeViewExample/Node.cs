44e987c39c5d59a0d6ddcb12609c5c2abfdfd5c4
Хасп Элд <EldHasp@Gmail.com>
2021-09-04 14:56:11 +0300

    Пример реализации показа TreeView из линейной коллекции.

---
+++ Examples/ExamplesSln/TreeViewExample/Node.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeViewExample
+{
+    public class Node
+    {
+        public int Id { get; }
+        public int ParentId { get; }
+
+        public string Title { get; }
+
+        public Node(int id, int parentId, string title)
+        {
+            Id = id;
+            ParentId = parentId;
+            Title = title;
+        }
+
+        public static ObservableCollection<Node> Nodes { get; }
+            = new ObservableCollection<Node>()
+            {
+                new Node(1, 0, "Первый"),
+                new Node(2, 0, "Второй"),
+                new Node(11, 1, "Третий"),
+                new Node(12, 1, "Четвёртый"),
+                new Node(13, 1, "Пятый"),
+                new Node(21, 2, "Шестой"),
+                new Node(111, 11, "Седьмой"),
+                new Node(121, 12, "Восьмой"),
+                new Node(211, 21, "Девятый"),
+            };
+
+    }
+}
---
+++ Examples/ExamplesSln/TreeViewExample/TagCollectionViewSource.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+using System.Windows.Data;
+
+namespace TreeViewExample
+{
+    public delegate void PropertyChangedHandler<T>(T sender, DependencyPropertyChangedEventArgs e)
+      where T : DependencyObject;
+    public class TagCollectionViewSource : CollectionViewSource
+    {
+        /// <summary>
+        /// Свойство для любых дополнительных данных.
+        /// </summary>
+        public object Tag
+        {
+            get => GetValue(TagProperty);
+            set => SetValue(TagProperty, value);
+        }
+
+        /// <summary><see cref="DependencyProperty"/> для свойства <see cref="Tag"/>.</summary>
+        public static readonly DependencyProperty TagProperty =
+            DependencyProperty.Register(
+                nameof(Tag),
+                typeof(object),
+                typeof(TagCollectionViewSource),
+                new PropertyMetadata(null));
+
+
+        /// <summary>
+        /// Делегат метода вызываемого при изменении свойства <see cref="Tag"/>.
+        /// </summary>
+        public PropertyChangedHandler<TagCollectionViewSource> PropertyChanged
+        {
+            get => (PropertyChangedHandler<TagCollectionViewSource>)GetValue(PropertyChangedProperty);
+            set => SetValue(PropertyChangedProperty, value);
+        }
+
+        /// <summary><see cref="DependencyProperty"/> для свойства <see cref="PropertyChanged"/>.</summary>
+        public static readonly DependencyProperty PropertyChangedProperty =
+            DependencyProperty.Register(
+                nameof(PropertyChanged),
+                typeof(PropertyChangedHandler<TagCollectionViewSource>),
+                typeof(TagCollectionViewSource),
+                new PropertyMetadata(null));
+
+        protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+            base.OnPropertyChanged(e);
+
+            PropertyChanged?.Invoke(this, e);
+        }
+
+    }
+}
